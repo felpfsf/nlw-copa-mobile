3a1a7b405262f1a14a2e4a36596e421f4a439b35
felpfsf <feferreira.sf@gmail.com>
2022-11-04 13:15:45 -0300

    feat: sign page

--- src/screens/SignIn/SignIn.tsx
+++ src/screens/SignIn/SignIn.tsx
@@ -1,10 +1,24 @@
-import { Center, Text } from 'native-base'
+import { Center, Icon, Text } from 'native-base'
+import { Fontisto } from '@expo/vector-icons'
+
+import Logo from '../../assets/logo.svg'
+import { Button } from '../../components/ui/Button'
 
 export function SignIn() {
   return (
-    <Center flex={1} bgColor='gray.900'>
-      <Text color={'white'} fontSize={24} fontWeight={'bold'}>
-        Fazer Login
+    <Center flex={1} bgColor='gray.900' p={7}>
+      <Logo width={212} height={40} />
+      <Button
+        title='Entrar com o google'
+        type='SECONDARY'
+        mt={12}
+        leftIcon={
+          <Icon as={Fontisto} name='google' color={'white'} size={'md'} />
+        }
+      />
+      <Text color='gray.200' fontSize='sm' textAlign='center' mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação
+        de sua conta.
       </Text>
     </Center>
   )
