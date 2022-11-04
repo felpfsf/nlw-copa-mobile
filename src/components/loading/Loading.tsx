7ffbe8e206b632df26cbe88a6e1ce576947efd8f
felpfsf <feferreira.sf@gmail.com>
2022-11-04 17:35:11 -0300

    feat: loading component

--- src/components/loading/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Center, Spinner } from 'native-base'
-
-export function Loading() {
-  return (
-    <Center flex={1} bg='gray.900'>
-      <Spinner color={'amber.500'} />
-    </Center>
-  )
-}
