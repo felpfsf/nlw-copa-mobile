c9cc5f6143e6ce856e03b0d594c42c1991c5992c
Felipe F <feferreira.sf@gmail.com>
2022-11-09 16:46:53 -0300

    feat: pools route to find

--- src/screens/Pools/Pools.tsx
+++ src/screens/Pools/Pools.tsx
@@ -4,8 +4,14 @@ import { Octicons } from '@expo/vector-icons'
 
 import { Button } from '../../components/ui/Button'
 import { Header } from '../../components/ui/Header'
+import { useNavigation } from '@react-navigation/native'
 
 export function Pools() {
+  const {navigate} = useNavigation()
+
+  function handleNavigation() {
+    navigate('find')
+  }
   return (
     <VStack flex={1} bg='gray.900'>
       <Header title='Meus bolões' />
@@ -15,12 +21,13 @@ export function Pools() {
           leftIcon={
             <Icon as={Octicons} name='search' color='black' size='md' />
           }
+          onPress={handleNavigation}
         />
       </VStack>
-        <Text color='gray.200' fontSize='sm' textAlign='center'  mt={4} px={12}>
-          Você ainda não está participando de nenhum bolão, que tal buscar um
-          por código ou criar um novo?
-        </Text>
+      <Text color='gray.200' fontSize='sm' textAlign='center' mt={4} px={12}>
+        Você ainda não está participando de nenhum bolão, que tal buscar um por
+        código ou criar um novo?
+      </Text>
     </VStack>
   )
 }
