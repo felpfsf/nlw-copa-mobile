471160978f72bf7ee4ad9791bed306490f7827bd
Felipe F <feferreira.sf@gmail.com>
2022-11-09 19:31:45 -0300

    feat: redirecting routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,18 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
+import { SignIn } from '../screens/SignIn/SignIn'
+
+import { useAuth } from '../hooks/useAuth'
+import { Box } from 'native-base'
 
 export function Routes() {
+  const { user } = useAuth()
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg='gray.900'>
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   )
 }
