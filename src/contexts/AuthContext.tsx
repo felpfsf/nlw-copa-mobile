717318a5b580084cc7c6494c1dadc52885be5b82
Felipe F <feferreira.sf@gmail.com>
2022-11-09 19:30:31 -0300

    feat: get token response user auth and user info

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,6 +2,7 @@ import { createContext, ReactNode, useState, useEffect } from 'react'
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session'
 import * as WebBrowser from 'expo-web-browser'
+import { api } from '../services/api'
 
 WebBrowser.maybeCompleteAuthSession()
 
@@ -47,6 +48,27 @@ export function AuthContextProvider({ children }: IAuthProviderProps) {
 
   async function signInWithGoogle(access_token: string) {
     console.log('Token de autenticação --> ', access_token)
+
+    try {
+      setIsUserLoading(true)
+
+      const tokenResponse = await api.post('/users', { access_token })
+      console.log(tokenResponse.data)
+
+      api.defaults.headers.common[
+        'Authorization'
+      ] = `Bearer ${tokenResponse.data.token}`
+
+      const userInfoResponse = await api.get('/me')
+      console.log(userInfoResponse.data)
+      setUser(userInfoResponse.data.user)
+
+    } catch (error) {
+      console.log(error)
+      throw error
+    } finally {
+      setIsUserLoading(false)
+    }
   }
 
   useEffect(() => {
