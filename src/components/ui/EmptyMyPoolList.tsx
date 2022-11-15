d5c61405f2eec1c1a1d534baa6274d782a1f5e49
Felipe F <feferreira.sf@gmail.com>
2022-11-15 20:08:55 -0300

    feat: sharecode function

--- src/components/ui/EmptyMyPoolList.tsx
+++ src/components/ui/EmptyMyPoolList.tsx
@@ -1,33 +1,46 @@
-import { Row, Text, Pressable } from 'native-base';
+import { Share } from 'react-native'
+import { Row, Text, Pressable } from 'native-base'
 
 interface Props {
-  code: string;
+  code: string
 }
 
 export function EmptyMyPoolList({ code }: Props) {
+  async function handleShareCode() {
+    Share.share({
+      message: code
+    })
+  }
   return (
-    <Row flexWrap="wrap" justifyContent="center" p={4}>
-      <Text color="gray.200" fontSize="sm">
-        Esse bolão ainda não tem participantes, que tal 
+    <Row flexWrap='wrap' justifyContent='center' p={4}>
+      <Text color='gray.200' fontSize='sm'>
+        Esse bolão ainda não tem participantes, que tal
       </Text>
 
-      <Pressable onPress={() => {}}>
-          <Text textDecorationLine="underline" color="yellow.500" textDecoration="underline">
+      <Pressable onPress={() => handleShareCode}>
+        <Text
+          textDecorationLine='underline'
+          color='yellow.500'
+          textDecoration='underline'>
           compartilhar o código
-          </Text>
+        </Text>
       </Pressable>
 
-      <Text color="gray.200" fontSize="sm" mx={1}>
+      <Text color='gray.200' fontSize='sm' mx={1}>
         do bolão com alguém?
       </Text>
 
-      <Text color="gray.200" mr={1}>
+      <Text color='gray.200' mr={1}>
         Use o código
       </Text>
-      
-      <Text color="gray.200" fontSize="sm" textAlign="center" fontFamily="heading"> 
+
+      <Text
+        color='gray.200'
+        fontSize='sm'
+        textAlign='center'
+        fontFamily='heading'>
         {code}
       </Text>
     </Row>
-  );
+  )
 }
