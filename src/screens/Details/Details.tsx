b73903ca3a2e1fb5a5a70ca398541f68f8e61acb
Felipe F <feferreira.sf@gmail.com>
2022-11-15 15:46:51 -0300

    feat: listing all games

--- src/screens/Details/Details.tsx
+++ src/screens/Details/Details.tsx
@@ -3,9 +3,11 @@ import { useRoute } from '@react-navigation/native'
 import { HStack, useToast, VStack } from 'native-base'
 import { Share } from 'react-native'
 
+import { api } from '../../services/api'
+
 import { Header } from '../../components/ui/Header'
 import { Loading } from '../../components/ui/Loading'
-import { api } from '../../services/api'
+import { Guesses } from '../../components/ui/Guesses'
 import { PoolCardProps } from '../../components/ui/PoolCard'
 import { PoolHeader } from '../../components/ui/PoolHeader'
 import { EmptyMyPoolList } from '../../components/ui/EmptyMyPoolList'
@@ -19,9 +21,9 @@ export function Details() {
   const toast = useToast()
   const route = useRoute()
   const { id } = route.params as IRouteParams
-  const [optionSelected, setOptionSelected] = useState<
-    'Seus Palpites' | 'Ranking do grupo'
-  >('Seus Palpites')
+  const [optionSelected, setOptionSelected] = useState<'guesses' | 'ranking'>(
+    'guesses'
+  )
   const [isLoading, setIsLoading] = useState(true)
   const [details, setDetails] = useState<PoolCardProps>({} as PoolCardProps)
 
@@ -68,18 +70,22 @@ export function Details() {
       {details._count?.participants > 0 ? (
         <VStack px={5} flex={1}>
           <PoolHeader data={details} />
+          
           <HStack bgColor='gray.800' p={1} rounded='sm' mb={5}>
             <Option
               title='Seus palpites'
-              isSelected={optionSelected === 'Seus Palpites'}
-              onPress={() => setOptionSelected('Seus Palpites')}
+              isSelected={optionSelected === 'guesses'}
+              onPress={() => setOptionSelected('guesses')}
             />
+
             <Option
-              title='Seus palpites'
-              isSelected={optionSelected === 'Ranking do grupo'}
-              onPress={() => setOptionSelected('Ranking do grupo')}
+              title='Ranking do grupo'
+              isSelected={optionSelected === 'ranking'}
+              onPress={() => setOptionSelected('ranking')}
             />
           </HStack>
+
+          <Guesses poolId={details.id} />
         </VStack>
       ) : (
         <EmptyMyPoolList code={details.code} />
